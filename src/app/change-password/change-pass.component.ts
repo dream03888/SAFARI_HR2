8a0de9e648d7ad6be0da070239c219aa02563c74
Dewvang <sittigon.yeevangtaku@hotmail.com>
2024-06-05 10:27:13 +0700

    edit chage pass

---
+++ src/app/change-password/change-pass.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-pass.component.html',
+  styleUrls: ['./change-pass.component.scss']
+})
+export class ChangePassComponent {
+
+}
