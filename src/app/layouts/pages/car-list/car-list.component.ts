8ae68f59956faf85df8d49a4928efca8db993869
Dewvang <sittigon.yeevangtaku@hotmail.com>
2024-06-27 15:42:04 +0700

    add delete function car list page

--- src/app/layouts/pages/car-list/car-list.component.ts
+++ src/app/layouts/pages/car-list/car-list.component.ts
@@ -8,26 +8,22 @@ import { Component } from '@angular/core';
 export class CarListComponent {
   isDeleteAlertVisible: boolean = false;
   isAnimating: boolean = false;
+  rowToDelete: any = null;
 
   tableData = Array.from({ length: 5 }, (_, index) => ({
     no: index + 1,
     departureDate: '03/01/2016 09:00',
-    // departureTime: '9:00',
     returnDate: '03/01/2016 09:00',
-    // returnTime: '9:00',
     car: 'Toyota Hilux Revo 2022 (3ขภ 9363)',
     location: 'xxxx',
-    // status: '1',
-    // status: '2',
     status: '3',
-    print: '',
-    choice: ''
-  })); 
-  
-  onAppClick(choice: string, event: MouseEvent): void {
+  }));
+
+  onAppClick(choice: string, row: any): void {
     if (choice === 'delete') {
+      this.rowToDelete = row;
       this.isDeleteAlertVisible = true;
-      this.isAnimating = false;   
+      this.isAnimating = false;
     }
   }
 
@@ -36,6 +32,11 @@ export class CarListComponent {
     this.isAnimating = true;
     setTimeout(() => {
       this.isAnimating = false;
-    }, 300); 
+    }, 300);
+  }
+
+  deleteRow(): void {
+    this.tableData = this.tableData.filter(row => row !== this.rowToDelete);
+    this.closeDeleteAlert();
   }
 }
